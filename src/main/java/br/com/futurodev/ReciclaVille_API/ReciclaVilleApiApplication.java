7172f2fc2dd7aaadadec0fc701035ab27879b728
Marcos Vinicius Santos da Silva <marcos_viny_12@outlook.com>
2025-05-05 20:46:13 -0300

    add: arquivos iniciais do projeto

---
+++ src/main/java/br/com/futurodev/ReciclaVille_API/ReciclaVilleApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.futurodev.ReciclaVille_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclaVilleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciclaVilleApiApplication.class, args);
+	}
+
+}
